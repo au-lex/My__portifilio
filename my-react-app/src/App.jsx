256bec9e5a91d4ba05516cc854d3ec28aed6393f
au-lex <aulexc4d2@gmail.com>
2023-08-18 05:37:19 +0100

    ui_component done

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,41 @@
 
 import './App.css'
+import Hero from './Components/Hero'
+import  { useState, useEffect } from 'react';
+import { InfinitySpin } from 'react-loader-spinner';
 
 function App() {
+  const [showLoader, setShowLoader] = useState(true);
+  useEffect(() => {
+      const timer = setTimeout(() => {
+          setShowLoader(false);
+      }, 1000);
+      return () => clearTimeout(timer);
+  }, []);
 
 
   return (
     <>
-      <h1 className='text-red-500'>hejjjllloooo</h1>
+  {showLoader ? (
+                <div className="loader ">
+                    <InfinitySpin
+                        height={400}
+                        width={400}
+                        radius={5}
+                        color="#2f84e4"
+                        ariaLabel="ball-triangle-loading"
+                        wrapperClass={{}}
+                        wrapperStyle=""
+                        visible={true}
+                    />
+                </div>
+            ) : (
+                <div>
+                  
+                </div>
+            )}
+    <Hero />
+  
     </>
   )
 }
